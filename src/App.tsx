c48aaa17df476767fbc02a961d4aa221603392dd
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-01-19 23:31:14 +0300

    add userlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import Card from './component/Card';
+import Card, { cardVariant } from './component/Card';
+import UserList from './component/UserList';
+import { IUser } from './types/types';
 
-const App =() => {
+const App = () => {
+  
+  const users: IUser[] = [
+    { id: 1, name: "Alex", email: "123@mail.ru", address:{city:"Moscow", street:"Lenina"} }
+]
+
   return <div>
-    <Card width='200px' height='200px'>
+    <Card onClick={(num) => console.log(num)} width='200px' height='200px' variant={cardVariant.primary}>
       <button>Привет</button>
     </Card>
+    <UserList users={users}/>
   </div>;
 }
 
--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-interface CardProps { 
+export enum cardVariant { 
+    outlined = 'outlined',
+    primary = 'primary'
+}
+
+interface CardProps {
     width?: string;
     height?: string;
     children?: React.ReactChild | React.ReactNode;
+    variant: cardVariant;
+    onClick: (num:number) => void;
 }
 
-export default function Card({ width, height, children}:CardProps) {
-    return (<div style={{ width, height, border: "2px solid gray" }}>
-        { children } 
-    </div>
-    )
+const Card: React.FC<CardProps> =
+    ({
+        width,
+        height,
+        variant,
+        onClick,
+        children
+    }) => {
 
-}
+
+const [state, setState] = useState(0)
+
+        return (<div style={{
+            width, height,
+            border: variant === cardVariant.outlined ? "2px solid gray" : 'none',
+            background: variant === cardVariant.primary ? 'lightgray' : ''
+        }}
+        onClick={() => onClick(state)}
+        >
+            {children}
+        </div>
+        )
+
+    }
+
+export default Card;
